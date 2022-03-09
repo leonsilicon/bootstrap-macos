51c7eb1ac296a241d5f25a2123708ffd02b9062a
Leon Si <contact@leonzalion.com>
2022-03-09 18:07:02 -0500

    fix: karabiner

--- src/bootstrap-scripts/karabiner-elements.ts
+++ src/bootstrap-scripts/karabiner-elements.ts
@@ -1,11 +1,14 @@
 import { brewInstall } from '~/utils/brew.js';
-import { sendMessage } from '~/utils/message.js';
-import { giveAppPermissionAccess, openSystemPreferencesPane } from '~/utils/system-preferences.js';
+import { giveAppPermissionAccess } from '~/utils/system-preferences.js';
 
 export async function installKarabinerElements() {
 	await brewInstall('karabiner-elements', { cask: true });
 	await giveAppPermissionAccess({
-		appName: 'karabiner_grabber'
+		appName: 'karabiner_grabber',
+		permissionName: 'Input Monitoring'
 	})
-	await
+	await giveAppPermissionAccess({
+		appName: 'karabiner_observer',
+		permissionName: 'Input Monitoring'
+	})
 }
