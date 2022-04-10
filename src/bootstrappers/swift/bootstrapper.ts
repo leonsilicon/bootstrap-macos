6123f9b8dba152b666c418bc4f1f4b282125ab55
Leon Si <contact@leonzalion.com>
2022-04-10 17:00:44 -0400

    update swift bootstrapper

--- src/bootstrappers/swift/bootstrapper.ts
+++ src/bootstrappers/swift/bootstrapper.ts
@@ -6,6 +6,10 @@ export const swiftBootstrapper = createBootstrapper({
 	bootstrap() {
 		// Mint is a swift formatter
 		await brewInstall('mint');
+		// https://stackoverflow.com/questions/61501298/xcrun-error-unable-to-find-utility-xctest-not-a-developer-tool-or-in-path
+		// mint install apple/swift-format@swift-5.3-branch
+		// install apple-swift-format vscode extension
+		// /Users/leonzalion/.mint/bin
 	},
 });
 
