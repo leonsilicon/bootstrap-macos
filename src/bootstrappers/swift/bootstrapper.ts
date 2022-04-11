0af710519c6bb9a499b695458196ab6607059be5
Leon Si <contact@leonzalion.com>
2022-04-10 22:37:50 -0400

    feat: swift bootstrapper

--- src/bootstrappers/swift/bootstrapper.ts
+++ src/bootstrappers/swift/bootstrapper.ts
@@ -1,15 +1,30 @@
 import { createBootstrapper } from '~/utils/bootstrapper.js';
+import { brewInstall } from '~/utils/brew.js';
+import { runCommand } from '~/utils/command.js';
+import { sendMessage } from '~/utils/message.js';
+import { pressToContinue } from '~/utils/prompt.js';
+import { addToZshrc } from '~/utils/zsh.js';
 
 export const swiftBootstrapper = createBootstrapper({
-	name: 'Heroku',
-	todo: true,
-	bootstrap() {
-		// Mint is a swift formatter
-		await brewInstall('mint');
-		// https://stackoverflow.com/questions/61501298/xcrun-error-unable-to-find-utility-xctest-not-a-developer-tool-or-in-path
-		// mint install apple/swift-format@swift-5.5-branch
-		// install apple-swift-format vscode extension
-		// /Users/leonzalion/.mint/bin
+	name: 'Swift',
+	async bootstrap(context) {
+		await sendMessage(context, 'Mint is a swift package management tool.');
+		await brewInstall(context, 'mint');
+
+		await pressToContinue(
+			context,
+			"when you've finished following the instructions at https://stackoverflow.com/questions/61501298/xcrun-error-unable-to-find-utility-xctest-not-a-developer-tool-or-in-path"
+		);
+
+		await runCommand(context, {
+			command: 'mint install apple/swift-format@swift-5.5-branch',
+		});
+
+		await addToZshrc(context, {
+			content: `export PATH="$PATH/Users/leonzalion/.mint/bin`,
+		});
+
+		// TODO: install apple-swift-format vscode extension
 	},
 });
 
